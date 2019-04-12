81ff9df9a670603c2404f198acbce8f0735e171b
Sharjeel Jan <sharjeel50@hotmail.co.uk>
2019-04-12 18:18:22 +0100

    changes made

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
+import os
 import json
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from includes.data import MovieData, SeriesData
 
 app = Flask(__name__)
@@ -16,10 +17,24 @@ def Movies():
 
 @app.route('/Movie/<string:id>/')
 def Movie(id):
-    for i in MovieData():
-        if str(i['ID']) == str(id):
-            Movie = i
+    Data = MovieData()
+    for i in range(50):
+        if Data[i]['ID'] == str(id):
+            Movie = Data[i]
     return render_template('Movie.html', Movie = Movie) # Running function to get movie data
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 
 if __name__ == '__main__':
--- includes/data.py
+++ includes/data.py
@@ -11,38 +11,10 @@ def SeriesData():
 
 
 def MovieData():
-
     with open('includes/MovieData.json','r') as f:
         Data = json.loads(f.read())
 
-    Movies = []
-
-    for i in range(100):
-        MovieDataDict = {
-                            'Title': Data[i]['Title'],
-                            'ID': Data[i]['ID'],
-                            'IMDb_ID': Data[i]['IMDb_ID'],
-                            'Year': Data[i]['Year'],
-                            'MovieInfo': Data[i]['MovieInfo'],
-                            'Genres': Data[i]['Genres'],
-                            'Certification': Data[i]['Certification'],
-                            'Percentage': Data[i]['Percentage'],
-                            'Votes': Data[i]['Votes'],
-                            'Watching': Data[i]['Watching'],
-                            'TenEighty': Data[i]['1080p'],
-                            'Filesize-1080p': Data[i]['Filesize-1080p'],
-                            'Peer-1080p': Data[i]['Peer-1080p'],
-                            'Seed-1080p': Data[i]['Seed-1080p'],
-                            'SevenTwenty': Data[i]['720p'],
-                            'Filesize-720p': Data[i]['Filesize-720p'],
-                            'Peer-720p': Data[i]['Peer-720p'],
-                            'Seed-720p': Data[i]['Seed-720p'],
-                            'Image': Data[i]['Image']
-                        }
-
-        Movies.append(MovieDataDict)
-
-    return Movies
+    return Data
 
 
 
