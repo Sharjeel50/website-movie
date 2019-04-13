2af3f37f4bb22f1dbdadb639970a281960abf608
Sharjeel Jan <sharjeel50@hotmail.co.uk>
2019-04-13 21:52:24 +0100

    fixed routes, fixed cards, added genre page and route

--- Main.py
+++ Main.py
@@ -9,11 +9,18 @@ app.static_folder = 'static'
 
 @app.route('/')
 def Home():
-    return render_template('Home.html', LoopMovies = MovieData()) # Running function to get movie data nad need to make sure i loop through series to display them too
+    Data = MovieData()
+    testMovies = []
+    for i in range(50):
+        testMovies.append(Data[i])
+
+    return render_template('Home.html', LoopMovies = testMovies) # Running function to get movie data nad need to make sure i loop through series to display them too
+
 
 @app.route('/Movies')
 def Movies():
     return render_template('Movies.html', LoopMovies = MovieData()) # Running function to get movie data
+
 
 @app.route('/Movie/<string:id>/')
 def Movie(id):
@@ -21,7 +28,20 @@ def Movie(id):
     for i in range(50):
         if Data[i]['ID'] == str(id):
             Movie = Data[i]
+
     return render_template('Movie.html', Movie = Movie) # Running function to get movie data
+
+
+@app.route('/Genre/<string:genre>')
+def GenrePage(genre):
+    Data = MovieData()
+    GenreMovies = []
+    for i in range(len(Data)):
+        if str(genre) in Data[i]['Genres']:
+            GenreMovies.append(Data[i])
+
+    return render_template('Genre.html', ChoseGenre = GenreMovies)
+
 
 @app.context_processor
 def override_url_for():
@@ -39,3 +59,16 @@ def dated_url_for(endpoint, **values):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+# def Genre(genre):
+#     Data = MovieData()
+#
+#     movies = []
+#
+#     for i in range(len(Data)):
+#         if genre in Data[i]['Genre']:
+#             movies.append(Data[i])
+#
+#     return render_template("dsffsd.html", loopThis = movies)
