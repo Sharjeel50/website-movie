ba63e2e2f0862d0b28911c57020fbb4c5150750b
Sharjeel50 <sharjeel50@hotmail.co.uk>
2019-04-15 01:31:27 +0100

    Login/Register added, need to configure backend

--- Main.py
+++ Main.py
@@ -3,9 +3,22 @@ import json
 import requests
 from flask import Flask, render_template, url_for
 from includes.data import MovieData, SeriesData
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.static_folder = 'static'
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def Home():
@@ -42,6 +55,35 @@ def GenrePage(genre):
     return render_template('Genre.html', ChoseGenre = GenreMovies)
 
 
+class LoginForm(FlaskForm):
+    username = StringField('username', validators = [InputRequired(), Length(min = 4, max = 15)])
+    password = PasswordField('password', validators = [InputRequired(), Length(min = 6, max = 15)])
+    remember = BooleanField('remember me')
+
+class RegisterFrom(FlaskForm):
+    username = StringField('Username', validators = [InputRequired(), Length(min = 4, max = 15)])
+    email = StringField('Email', validators = [InputRequired(), Email(message = 'Invalid Email'), Length(max = 30)])
+    password = StringField('Password', validators = [InputRequired(), Length(min = 6, max = 15)])
+
+
+@app.route("/Login")
+def Login():
+    form = LoginForm()
+    return render_template('Login.html', form = form)
+
+@app.route("/Register", methods = ['GET', 'POST'])
+def Register():
+    form = RegisterFrom()
+    if request.method == 'POST':
+        if mongo.db.users.find_one({'username': request.form['Username']}) is None:
+            pass
+
+
+
+    return render_template('Register.html', form = form)
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 @app.context_processor
 def override_url_for():
     return dict(url_for=dated_url_for)
@@ -58,16 +100,3 @@ def dated_url_for(endpoint, **values):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-# def Genre(genre):
-#     Data = MovieData()
-#
-#     movies = []
-#
-#     for i in range(len(Data)):
-#         if genre in Data[i]['Genre']:
-#             movies.append(Data[i])
-#
-#     return render_template("dsffsd.html", loopThis = movies)
