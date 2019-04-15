31da72fead1a66cf0cecacd7d7bcd3f89271c03b
Sharjeel50 <sharjeel50@hotmail.co.uk>
2019-04-15 20:07:43 +0100

    register and login partially working, added more templates

--- Main.py
+++ Main.py
@@ -1,24 +1,27 @@
 import os
 import json
+import hashlib
 import requests
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from includes.data import MovieData, SeriesData
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, validators, Form
 from wtforms.validators import InputRequired, Email, Length
-from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.static_folder = 'static'
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Users"
 mongo = PyMongo(app)
 
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
 bootstrap = Bootstrap(app)
+
+# Home
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 @app.route('/')
 def Home():
@@ -29,12 +32,42 @@ def Home():
 
     return render_template('Home.html', LoopMovies = testMovies) # Running function to get movie data nad need to make sure i loop through series to display them too
 
-@app.route('/Movies')
+
+# Movies Page
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+@app.route('/Movies') # TODO
 def Movies():
     return render_template('Movies.html', LoopMovies = MovieData()) # Running function to get movie data
 
 
-@app.route('/Movie/<string:id>/')
+# Series Page
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+@app.route('/Serie/<string:id>') # TODO
+def Serie():
+
+        # Data = MovieData()
+        # for i in range(50):
+        #     if Data[i]['ID'] == str(id):
+        #         Movie = Data[i]
+        #
+
+    return render_template('Series.html') # Running function to get movie data
+
+
+# Individual Serie Page
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+@app.route('/Series') #TODO
+def Series():
+    return render_template('Series.html') # Running function to get movie data
+
+
+# Individual Movie Page
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+@app.route('/Movie/<genre>/') # To fix once the user clicks on next button to
 def Movie(id):
     Data = MovieData()
     for i in range(50):
@@ -43,6 +76,8 @@ def Movie(id):
 
     return render_template('Movie.html', Movie = Movie) # Running function to get movie data
 
+# Genre Page
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 @app.route('/Genre/<string:genre>')
 def GenrePage(genre):
@@ -54,34 +89,69 @@ def GenrePage(genre):
 
     return render_template('Genre.html', ChoseGenre = GenreMovies)
 
+# Login
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-class LoginForm(FlaskForm):
+class LoginForm(Form):
     username = StringField('username', validators = [InputRequired(), Length(min = 4, max = 15)])
     password = PasswordField('password', validators = [InputRequired(), Length(min = 6, max = 15)])
     remember = BooleanField('remember me')
 
-class RegisterFrom(FlaskForm):
-    username = StringField('Username', validators = [InputRequired(), Length(min = 4, max = 15)])
-    email = StringField('Email', validators = [InputRequired(), Email(message = 'Invalid Email'), Length(max = 30)])
-    password = StringField('Password', validators = [InputRequired(), Length(min = 6, max = 15)])
-
-
 @app.route("/Login")
 def Login():
-    form = LoginForm()
-    return render_template('Login.html', form = form)
+    form = LoginForm(request.form)
+    return render_template('Login.html', form = form)  # Need to make sure user gets logged in on username and password verification then redirect
+
+
+# Register
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class RegisterFrom(Form):
+    username = StringField('Username', validators = [InputRequired(), Length(min = 4, max = 15)])
+    email = StringField('Email', validators = [InputRequired(), Email(message = 'Invalid Email'), Length(max = 30)])
+    password = StringField('Password', validators = [InputRequired(), PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('Confirm', message = "Passwords do not match")]),Length(min = 6, max = 15)])
+    confirm = PasswordField('Confirm Password')
 
 @app.route("/Register", methods = ['GET', 'POST'])
 def Register():
-    form = RegisterFrom()
-    if request.method == 'POST':
-        if mongo.db.users.find_one({'username': request.form['Username']}) is None:
-            pass
+    usersDatabase = mongodb.db.Users
+    form = RegisterFrom(request.form)
+    if request.method == 'POST' and form.validate() and usersDatabase.find_one({'Username': form.username.data}) is None:
+        username = form.username.data
+        email = form.email.data
+        password = hashlib.sha256(form.password.data.encode('utf-8')).hexdigest()
+        usersDatabase.insert({'Username': username, 'Email': email, 'Password': password})
 
 
-
+        return render_template('Register.html', form = form)  # Need to add to database if user does not exists within it
     return render_template('Register.html', form = form)
 
+       # if mongo.db.users.find_one({'username': request.form['Username']}) is None:
+           # pass
+
+    #return render_template('Register.html', form = form)
+
+
+
+# Contact us Page
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Contact_Us(Form):
+    title = StringField('title', validators = [InputRequired(), Length(min = 5, max = 50)])
+    content = StringField('content', validators = [InputRequired(), Length(max = 100)])
+
+@app.route('/ContactUs', methods = ['GET', 'POST'])
+def Contactus():
+    form = RegisterFrom(request.form)
+    if request.method = 'POST' and form.validate():
+        return render_template('ContactUs.html', form = form)
+    return render_template('ContactUs.html', form = form)
+
+
+
+# Dont mess with this shit
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 @app.context_processor
--- includes/data.py
+++ includes/data.py
@@ -8,10 +8,19 @@ def SeriesData():
     SeriesLoadedData = json.loads(SeriesDumpedData)
 
     return Series
-
+##
+##def Movies():
+##    SeriesURL = "https://tv-v2.api-fetch.website/movies"
+##    SeriessResponse = requests.get(SeriesURL, headers= {'Content-Type': 'application/json; charset=utf-8'})
+##    SeriesDumpedData = json.dumps(SeriessResponse.json())
+##    SeriesLoadedData = json.loads(SeriesDumpedData)
+##
+##    print(SeriesLoadedData)
+    
 
 def MovieData():
     with open('includes/MovieData.json','r') as f:
         Data = json.loads(f.read())
 
     return Data
+
