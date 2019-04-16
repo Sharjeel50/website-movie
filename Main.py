3af9ed67069118aad5915c5c341564d84f93ea40
Sharjeel Jan <sharjeel50@hotmail.co.uk>
2019-04-16 22:40:01 +0100

    Register working, Login needs to be addressed as well as passowrd decryption

--- Main.py
+++ Main.py
@@ -2,18 +2,20 @@ import os
 import json
 import hashlib
 import requests
-from flask import Flask, render_template, url_for, request, redirect
-from includes.data import MovieData, SeriesData
-from wtforms import StringField, PasswordField, BooleanField, validators
-from flask_wtf import Form
-from wtforms.validators import InputRequired, Email, Length
-from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
 from flask_pymongo import PyMongo
+from passlib.hash import sha256_crypt
+from flask_bootstrap import Bootstrap
+from includes.data import MovieData, SeriesData
+from wtforms.validators import InputRequired, Email, Length, EqualTo, DataRequired
+from flask import Flask, render_template, url_for, request, redirect
+from wtforms import StringField, PasswordField, BooleanField, validators, SubmitField
+
 
 app = Flask(__name__)
 app.static_folder = 'static'
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/Users"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/PineFlix"
 mongodb = PyMongo(app)
 
 SECRET_KEY = os.urandom(32)
@@ -91,62 +93,79 @@ def GenrePage(genre):
 
     return render_template('Genre.html', ChoseGenre = GenreMovies)
 
-# Login
-# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-class LoginForm(Form):
-    username = StringField('username', validators = [InputRequired(), Length(min = 4, max = 15)])
-    password = PasswordField('password', validators = [InputRequired(), Length(min = 6, max = 15)])
-    remember = BooleanField('remember me')
-
-@app.route("/Login")
-def Login():
-    form = LoginForm(request.form)
-    return render_template('Login.html', form = form)  # Need to make sure user gets logged in on username and password verification then redirect
 
 
 # Register
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-class RegisterFrom(Form):
-    username = StringField('Username', validators = [InputRequired(), Length(min = 4, max = 15)])
-    email = StringField('Email', validators = [InputRequired(), Email(message = 'Invalid Email'), Length(max = 30)])
-    password = StringField('Password', validators = [InputRequired(), PasswordField('Password', [validators.DataRequired(), validators.EqualTo('Confirm', message = "Passwords do not match")]),Length(min = 6, max = 15)])
-    confirm = PasswordField('Confirm Password')
+class RegisterFrom(FlaskForm):
+    username = StringField('Username', validators = [DataRequired(), Length(min = 2, max = 15)])
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    confirm = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 @app.route("/Register", methods = ['GET', 'POST'])
 def Register():
     usersDatabase = mongodb.db.Users
     form = RegisterFrom(request.form)
-    if request.method == 'POST' and form.validate_on_submit():
-        username = form.username.data
-        email = form.email.data
-        password = hashlib.sha256(form.password.data.encode('utf-8')).hexdigest()
-        usersDatabase.insert({'Username': username, 'Email': email, 'Password': password})
 
-        print("User Added to database")
+    if usersDatabase.find_one({'Email': form.email.data }) is None:
+        if request.method == 'POST' and form.validate_on_submit():
+            username = form.username.data
+            email = form.email.data
+            password = sha256_crypt.encrypt(str(form.password.data))
+            usersDatabase.insert({'Username': username, 'Email': email, 'Password': password})
 
-        return redirect(url_for('Home'))
+            print("User Added to database")
+
+            return redirect(url_for('Home'))
 
     return render_template('Register.html', form = form)
 
-       # if mongo.db.users.find_one({'username': request.form['Username']}) is None:
-           # pass
 
-    #return render_template('Register.html', form = form)
+# Login
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')
+
+@app.route("/Login", methods = ['GET', 'POST'])
+def Login():
+    form = LoginForm()
+    usersDatabase = mongodb.db.Users
+
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data
+        password = form.email.data
+        a = usersDatabase.find_one({'Email': email})
+        print(a['Password'])
+
+
+        if sha256_crypt.verify(a['Password'], password):
+            print("logged in")
+            return redirect(url_for('Register'))
+    else:
+        #return redirect(url_for('Home'))
+        print("no user")
+
+    return render_template('Login.html', form = form)  # Need to make sure user gets logged in on username and password verification then redirect
 
 
 
 # Contact us Page
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-class Contact_Us(Form):
+class Contact_Us(FlaskForm):
     title = StringField('title', validators = [InputRequired(), Length(min = 5, max = 50)])
     content = StringField('content', validators = [InputRequired(), Length(max = 100)])
 
 @app.route('/ContactUs', methods = ['GET', 'POST'])
 def Contactus():
-    form = RegisterFrom(request.form)
+    form = RegisterFrom()
     # if request.method = 'POST' and form.validate():
     #     return render_template('ContactUs.html', form = form)
     return render_template('ContactUs.html', form = form)
